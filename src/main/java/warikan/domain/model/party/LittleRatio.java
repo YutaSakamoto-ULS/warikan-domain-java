e521cd4fee9222ba6dbf4f080b8f54832f145d2e
yuta.sakamoto <yuta.sakamoto@ulsystems.co.jp>
2022-02-07 11:07:17 +0900

    LittleRatioの制約を「100以下」で追加

--- src/main/java/warikan/domain/model/party/LittleRatio.java
+++ src/main/java/warikan/domain/model/party/LittleRatio.java
@@ -1,11 +1,12 @@
 package warikan.domain.model.party;
 
 import java.math.BigDecimal;
+import java.security.InvalidParameterException;
 import java.util.Objects;
 
 import javax.annotation.Nonnull;
 
-/** 弱者控除額 */
+/** 弱者控除割合 */
 public final class LittleRatio {
   private final BigDecimal value;
   private LittleRatio(@Nonnull BigDecimal value) {
@@ -15,12 +16,16 @@ public final class LittleRatio {
   /**
    * ファクトリメソッド。
    *
-   * @param value 弱者控除額。
+   * @param value 弱者控除割合(パーセント) 
    * @return {@link LittleRatio}
    */
   @Nonnull
   public static LittleRatio of(@Nonnull BigDecimal value) {
-    return new LittleRatio(value);
+    BigDecimal ONE_HANDLED = BigDecimal.valueOf(100);
+    if(value.compareTo(ONE_HANDLED) == -1){
+      throw new InvalidParameterException("弱者控除割合");
+    }
+    return new LittleRatio(value.divide(ONE_HANDLED));
   }
 
   @Nonnull
